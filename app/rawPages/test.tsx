44abf46d8e98ecebf279a00a15043d6f6e00f8df
Srichandan Mohapatra <79102175+Jc-cell-cpu@users.noreply.github.com>
2025-06-23 11:22:59 +0530

    feat: add test page with HomePageSkeleton component

---
+++ app/rawPages/test.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import HomePageSkeleton from "@/components/HomePageSkeleton";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const test = () => {
+  return <HomePageSkeleton />;
+};
+
+export default test;
+
+const styles = StyleSheet.create({});
